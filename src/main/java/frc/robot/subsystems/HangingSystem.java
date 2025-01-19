1718bd5d60c06d8dab183f005fd999ddbdc907b4
talya <talya.duani123@gmail.com>
2025-01-19 18:35:04 +0200

    added hanging system

---
+++ src/main/java/frc/robot/subsystems/HangingSystem.java
@@ -0,0 +1,60 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.wpilibj.Servo;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.RobotMap;
+
+import javax.swing.*;
+
+public class HangingSystem extends SubsystemBase {
+    private Servo motorR;
+    private Servo motorL;
+
+
+    public HangingSystem(){
+        motorR =  new Servo(RobotMap.HANGING_MOTOR_RIGHT_ID);
+        motorL =  new Servo(RobotMap.HANGING_MOTOR_LEFT_ID);
+    }
+
+    public void openRight(){
+        motorR.setAngle(RobotMap.HANGING_OPEN_ANGLE);
+    }
+
+    public void openLeft(){
+        motorL.setAngle(RobotMap.HANGING_OPEN_ANGLE);
+    }
+
+    public void closeLeft(){
+        motorL.setAngle(RobotMap.HANGING_CLOSE_ANGLE);
+    }
+
+    public void closeRight(){
+        motorR.setAngle(RobotMap.HANGING_CLOSE_ANGLE);
+    }
+
+    public double getLeftAngle(){
+        return motorL.getAngle();
+    }
+
+    public double getRightAngle(){
+        return motorR.getPosition();
+    }
+
+    public boolean didGetToAngleLeft(double targetAngle){
+        return MathUtil.isNear(targetAngle, getLeftAngle(), 0.5);
+    }
+
+    public boolean didGetToAngleRight(double targetAngle){
+        return MathUtil.isNear(targetAngle, getRightAngle(), 0.5);
+    }
+
+    public void stopLeft(){
+        motorL.setDisabled();
+    }
+
+    public void stopRight(){
+        motorR.setDisabled();
+    }
+
+}
